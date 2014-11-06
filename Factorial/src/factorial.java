eff3fd244dc3fd728e7fb4632f964a0c80e5ad82
Roger <Roger@Roger>
2014-11-06 17:43:46 +0100

    Versió inicial

---
+++ Factorial/src/factorial.java
@@ -0,0 +1,28 @@
+/**
+ * Classe que calcula el factorial d’un nombre.
+ *
+ * @author Roger Girbes
+ * @version 2014
+ */
+public class factorial {
+	/**
+	 * Calcula el factorial de n. n! = n * (n-1) * (n-2) * (n-3) * ... * 1
+	 * 
+	 * @param n
+	 *            és el número al que es calcularà el factorial.
+	 * @return n! és el resultat del factorial de n
+	 */
+	public static double factorial(double n) {
+		if (n == 0)
+			return 1;
+		else {
+			double resultat = n * factorial(n - 1);
+			return resultat;
+		}
+	}
+	
+	public static void main (String [] args){
+		System.out.println(factorial(5));
+	}
+
+}
